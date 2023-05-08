63a6b0b1851ebbbfdd8f4cbccbeaf41640438a48
Volkmar Rigo <volkmar@rigo.bz>
2023-05-08 16:29:32 +0200

    Added first version  of IsPrimeNumber and Tests

--- Prime.Tests/PrimeNumbersTests.cs
+++ Prime.Tests/PrimeNumbersTests.cs
@@ -12,4 +12,39 @@ public class PrimeNumbersTests
         Action act = () => PrimeNumbers.IsPrimeNumber(-1);
         act.Should().Throw<ArgumentException>();
     }
+
+    [Fact]
+    public void Number1ShoudNotBeAPrimeNumberTest()
+    {
+        PrimeNumbers.IsPrimeNumber(1).Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(5)]
+    [InlineData(7)]
+    [InlineData(11)]
+    [InlineData(13)]
+    [InlineData(17)]
+    [InlineData(19)]
+    [InlineData(23)]
+    [InlineData(29)]
+    public void NumberShoudBeAPrimeNumberTest(int number)
+    {
+        PrimeNumbers.IsPrimeNumber(number).Should().BeTrue();
+    }
+
+
+    [Theory]
+    [InlineData(4)]
+    [InlineData(6)]
+    [InlineData(8)]
+    [InlineData(9)]
+    [InlineData(10)]
+    public void NumberShoudNotBeAPrimeNumberTest(int number)
+    {
+        PrimeNumbers.IsPrimeNumber(number).Should().BeFalse();
+    }
+
 }
--- Prime/PrimeNumbers.cs
+++ Prime/PrimeNumbers.cs
@@ -10,7 +10,19 @@ public static class PrimeNumbers
 {
     public static bool IsPrimeNumber(int number)
     {
-        return false;
+        if (number < 1)
+            throw new ArgumentException("0 or negative numbers are invalid arguments");
+
+        if (number == 1)
+            return false;
+
+        for (int i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
     }
 
     public static IEnumerable<int> Get(int fromNumber, int toNumber)
